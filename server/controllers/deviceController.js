de2bda5299b54a1cb3bd2417eeb808e754e12a9e
Nikita <poker6-96@mail.com>
2022-10-01 23:58:07 +0300

    fix security issue

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,19 @@
+class DeviceController {
+    async registration(req, res) {
+
+    }
+
+    async login(req, res) {
+
+    }
+
+    async check(req, res) {
+
+    }
+
+    async getOne(req, res) {
+
+    }
+}
+
+module.exports = new DeviceController()
