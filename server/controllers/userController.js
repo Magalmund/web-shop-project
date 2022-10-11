da792e40127bad08bc37fe274191334705099ed4
Kaloid <poker6-96@mail.ru>
2022-10-11 15:22:25 +0300

    get devices, pagination, limit + registration

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,23 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
+
 
 class UserController {
-    async registration(req, res) {
-
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if(!email || !password) {
+            return next(ApiError.badRequest('Invalid email or password'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if(candidate) {
+            return next(ApiError.badRequest(('User with this email already exist')))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, role, password: hashPassword})
+        const basket = await Basket. create({userId: user.id})
+        const jwt = jwt.sign({id: user.id, email, role}, )
     }
 
     async login(req, res) {
